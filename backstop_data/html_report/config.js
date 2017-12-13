881cff1e1fe1bd201829643b873363eadb29c187
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2017-12-13 11:11:53 +0000

    updates

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,26 +4,44 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
-        "test": "../bitmaps_test/20171212-153416/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20171212-211814/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
         "selector": "document",
         "fileName": "backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
         "label": "BackstopJS Homepage",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
-        "error": "Reference file not found /Users/dilwoarhussain/Sites/GDS-Multi-Faith-Room/backstop_data/bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_0_phone.png"
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -150,
+            "height": 78
+          },
+          "misMatchPercentage": "23.06",
+          "analysisTime": 127
+        },
+        "diffImage": "../bitmaps_test/20171212-211814/failed_diff_backstop_default_BackstopJS_Homepage_0_document_0_phone.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20171212-153416/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20171212-211814/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
         "selector": "document",
         "fileName": "backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
         "label": "BackstopJS Homepage",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
-        "error": "Reference file not found /Users/dilwoarhussain/Sites/GDS-Multi-Faith-Room/backstop_data/bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png"
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.16",
+          "analysisTime": 50
+        },
+        "diffImage": "../bitmaps_test/20171212-211814/failed_diff_backstop_default_BackstopJS_Homepage_0_document_1_tablet.png"
       },
       "status": "fail"
     }
