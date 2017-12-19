aaffc70e28345b6bfea3ff906f9928d97736e093
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2017-12-19 16:20:01 +0000

    updates to designs and banners

---
+++ resources/views/includes/beta-warning.blade.php
@@ -0,0 +1,6 @@
+<div class="phase-banner">
+  <p>
+    <strong class="phase-tag">BETA</strong>
+    <span>This is a new service – your <a href="https://github.com/DilwoarH/GDS-Multi-Faith-Room/issues/new">feedback</a> will help us to improve it.</span>
+  </p>
+</div>
---
+++ resources/views/includes/layout.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Multi Faith Room - Booking System</title>
+        <link rel="stylesheet" href="/css/app.css">
+        
+    </head>
+    <body>
+        @yield('content')
+        
+        <script src="/js/app.js"></script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109839238-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-109839238-1');
+        </script>
+    </body>
+</html>
