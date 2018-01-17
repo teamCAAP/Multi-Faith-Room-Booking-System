5171ede1ee61f0174cb71508c16c229c017a24c6
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2018-01-17 10:50:48 +0000

    backstop update

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -3,33 +3,11 @@ report({
   "tests": [
     {
       "pair": {
-        "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
-        "test": "../bitmaps_test/20171212-211814/backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
+        "reference": "../bitmaps_reference/backstop_default_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180117-104938/backstop_default_Homepage_0_document_0_phone.png",
         "selector": "document",
-        "fileName": "backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
-        "label": "BackstopJS Homepage",
-        "requireSameDimensions": true,
-        "misMatchThreshold": 0.1,
-        "diff": {
-          "isSameDimensions": false,
-          "dimensionDifference": {
-            "width": -150,
-            "height": 78
-          },
-          "misMatchPercentage": "23.06",
-          "analysisTime": 127
-        },
-        "diffImage": "../bitmaps_test/20171212-211814/failed_diff_backstop_default_BackstopJS_Homepage_0_document_0_phone.png"
-      },
-      "status": "fail"
-    },
-    {
-      "pair": {
-        "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20171212-211814/backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
-        "selector": "document",
-        "fileName": "backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
-        "label": "BackstopJS Homepage",
+        "fileName": "backstop_default_Homepage_0_document_0_phone.png",
+        "label": "Homepage",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "diff": {
@@ -38,12 +16,72 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.16",
-          "analysisTime": 50
-        },
-        "diffImage": "../bitmaps_test/20171212-211814/failed_diff_backstop_default_BackstopJS_Homepage_0_document_1_tablet.png"
+          "misMatchPercentage": "0.01",
+          "analysisTime": 147
+        }
       },
-      "status": "fail"
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180117-104938/backstop_default_Homepage_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Homepage_0_document_1_tablet.png",
+        "label": "Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.01",
+          "analysisTime": 143
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Admin_Screen_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180117-104938/backstop_default_Admin_Screen_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Admin_Screen_0_document_0_phone.png",
+        "label": "Admin Screen",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Admin_Screen_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180117-104938/backstop_default_Admin_Screen_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Admin_Screen_0_document_1_tablet.png",
+        "label": "Admin Screen",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
     }
   ]
 });
